720f56b997a80a83837ae72d537a96f65ada76e7
Kai Hoppert <kai.hoppert@tomcom.de>
2013-12-03 08:36:31 +0100

    first commit

---
+++ README.rst
@@ -0,0 +1,17 @@
+Information
+===========
+
+Bootstrap http://getbootstrap.com/
+
+Bootstrap css and js integration for plone
+
+Copyrights and licenses
+=======================
+
+Bootstrap
+
+- Copyright 2012 Twitter, Inc.
+- Licensed under the `Apache License v2.0 <http://www.apache.org/licenses/LICENSE-2.0>`_.
+- See DOCS-LICENSE, LICENSE, LICENSE-MIT for bootstrap license
+
+We changed the font path in the css files for a clean plone integration.
---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+version = '3.0.2'
+
+tests_require = [
+    'plone.app.testing',
+    'pyquery'
+    ]
+
+setup(name='tomcom.plone.bootstrap',
+      version=version,
+      description='bootstrap for plone',
+      long_description=open("README.rst").read() + '\n' +
+                       open('CHANGES.rst').read(),
+      classifiers=[
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.3',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+      keywords='font awesome',
+      author='tomcom GmbH',
+      author_email='mailto:info@tomcom.de',
+      url='https://github.com/tomcom-de/tomcom.plone.bootstrap',
+      license='GPL',
+      packages=find_packages(),
+      namespace_packages=['tomcom','tomcom.plone'],
+      include_package_data=True,
+      install_requires=[
+        'setuptools',
+      ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require,
+                     ),
+      zip_safe=False,
+      entry_points='''
+[z3c.autoinclude.plugin]
+target = plone
+''',
+)
